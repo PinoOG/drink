ae37fdacca10c50bb52a9c3c07626658980445d4
Pino <mrcreeper1216@gmail.com>
2025-03-21 21:34:52 +0100

    Debugging vanish problem

--- src/main/java/com/jonahseguin/drink/provider/spigot/PlayerProvider.java
+++ src/main/java/com/jonahseguin/drink/provider/spigot/PlayerProvider.java
@@ -85,7 +85,12 @@ public class PlayerProvider extends DrinkProvider<Player> {
         System.out.println("Players: " + plugin.getServer().getOnlinePlayers());
         final List<String> players =  plugin.getServer().getOnlinePlayers()
                 .stream()
-                //.filter(player -> !player.hasMetadata("vanished"))
+                .filter(player -> {
+                    if(player.hasMetadata("vanished")) {
+                        System.out.println("Player is vanished: " + player.getName());
+                    }
+                    return !player.hasMetadata("vanished");
+                })
                 .map(HumanEntity::getName)
                 .filter(s -> finalPrefix.isEmpty() || s.toLowerCase().startsWith(finalPrefix))
                 .collect(Collectors.toCollection(CopyOnWriteArrayList::new));
